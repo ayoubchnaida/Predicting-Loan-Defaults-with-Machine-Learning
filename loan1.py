2f43ed0e0354f062dc7bd483b0a1079ee7ddca0f
CHNAIDA AYOUB <131295282+ayoubchnaida@users.noreply.github.com>
2023-10-12 13:43:24 +0100

    Update loan1.py

--- loan1.py
+++ loan1.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pickle
 import pandas as pd
 import sklearn
-from xgboost import XGBClassifier
 # Load the trained model
 model = pickle.load(open('loan.sav', 'rb'))
 # Streamlit web app
